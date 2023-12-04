2d0537e63cab2a9d67f0e1ee5dba892dcd808ef3
Niklas Röttger <niklas.roettger@outlook.de>
2023-12-04 14:38:04 +0100

    add create route and addplace function

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,8 +5,23 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const places = await Place.find();
-    return response.status(200).json(places);
+    try {
+      const places = await Place.find();
+      return response.status(200).json(places);
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  } else if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Place.create(placeData);
+
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   } else {
     return response.status(405).json({ message: "Method not allowed" });
   }
